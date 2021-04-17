d1af002a02ba94645958a251bfd687719cb2a700
Mark <mfraz74+git@gmail.com>
2021-04-17 15:34:10 +0100

    Update silly_clock.py

--- examples/silly_clock.py
+++ examples/silly_clock.py
@@ -44,8 +44,8 @@ def animation(device, from_y, to_y):
 def main():
     # Setup for Banggood version of 4 x 8x8 LED Matrix (https://bit.ly/2Gywazb)
     serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(serial, cascaded=4, block_orientation=-90, blocks_arranged_in_reverse_order=True)
-    device.contrast(16)
+    device = max7219(serial, cascaded=4, block_orientation=90, blocks_arranged_in_reverse_order=True)
+    device.contrast(1)
 
     # The time ascends from the abyss...
     animation(device, 8, 1)
@@ -53,14 +53,17 @@ def main():
     toggle = False  # Toggle the second indicator every second
     while True:
         toggle = not toggle
-        sec = datetime.now().second
+        now = datetime.now()
+        sec = now.second
+        hour = now.hour
+        minute =  now.minute
         if sec == 59:
             # When we change minutes, animate the minute change
             minute_change(device)
-        elif sec == 30:
+        elif (hour > 7 and hour < 19) and minute ==0 and sec == 0:
             # Half-way through each minute, display the complete date/time,
             # animating the time display into and out of the abyss.
-            full_msg = time.ctime()
+            full_msg = now.strftime('%a %H.%M %d/%m/%y')
             animation(device, 1, 8)
             show_message(device, full_msg, fill="white", font=proportional(CP437_FONT))
             animation(device, 8, 1)
@@ -68,12 +71,10 @@ def main():
             # Do the following twice a second (so the seconds' indicator blips).
             # I'd optimize if I had to - but what's the point?
             # Even my Raspberry PI2 can do this at 4% of a single one of the 4 cores!
-            hours = datetime.now().strftime('%H')
-            minutes = datetime.now().strftime('%M')
             with canvas(device) as draw:
-                text(draw, (0, 1), hours, fill="white", font=proportional(CP437_FONT))
+                text(draw, (0, 1), f'{hour:02d}', fill="white", font=proportional(CP437_FONT))
                 text(draw, (15, 1), ":" if toggle else " ", fill="white", font=proportional(TINY_FONT))
-                text(draw, (17, 1), minutes, fill="white", font=proportional(CP437_FONT))
+                text(draw, (17, 1), f'{minute:02d}', fill="white", font=proportional(CP437_FONT))
             time.sleep(0.5)
 
 
